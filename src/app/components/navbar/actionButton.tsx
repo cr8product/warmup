0e48ea5afc1c2d606d621622ef0b9ddcea22bda2
Metin <aknmetin@gmail.com>
2023-08-02 09:03:57 +0000

    Added network switcher

--- src/app/components/navbar/actionButton.tsx
+++ src/app/components/navbar/actionButton.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Web3Button, Web3Modal } from "@web3modal/react";
+import { Web3Button, Web3Modal, Web3NetworkSwitch } from "@web3modal/react";
 import { WagmiConfig } from "wagmi";
 import { ethereumClient, projectId, chains, wagmiConfig } from '@/app/wagmi-config/config'
 
@@ -8,7 +8,9 @@ export default function ActionButton() {
     <>
       <WagmiConfig config={wagmiConfig}>
         <a className="btn">Button</a>
+        <Web3NetworkSwitch />
         <Web3Button />
+        
       </WagmiConfig>
 
       <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
