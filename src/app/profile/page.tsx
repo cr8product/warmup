416be31aeeaef1682a9f7114fd89b2209d4eb1fc
Metin <aknmetin@gmail.com>
2023-08-15 10:42:59 +0000

    Event handling and adding a gitignore

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,19 @@
+"use client"
 import axios from "axios"
 import { useRouter } from "next/navigation"
 
 
 export default function Profile() {
+    const router = useRouter();
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            router.push("login")
+        } catch (error:any) {
+            console.log(error.message)
+        }
+    }
     
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -10,7 +21,7 @@ export default function Profile() {
             <hr />
             <p>Profile page</p>
             <hr />
-            <button className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+            <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Logout</button>
         </div>
     )
 }
